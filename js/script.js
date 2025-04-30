7e3ee99cb64721ac1d7691e524861b149e89e2b0
jaypetlele <jaypetlele@gmail.comm>
2025-04-30 16:22:59 +0200

    Add the logic to play a single round

--- js/script.js
+++ js/script.js
@@ -5,29 +5,52 @@
 function getComputerChoice (){
     let x = Math.floor(Math.random() * 3);
    
-   if (x == 0){
+   if (x === 0){
     return "Rock";
-} else if (x == 1){
+} else if (x === 1){
     return "Paper";
-} else if (x == 2){
-    return "Scissors"
+} else if (x === 2){
+    return "Scissor"
 };
 }
 
-/*console.log(getComputerChoice())*/
+
 
 /* Generates Human Choice and returns it */
 function getHumanChoice (){
-    let userInput = prompt("Please choose Rock, Paper or Scissors:")
+    let userInput = prompt("Please choose Rock, Paper or Scissor:").toLocaleLowerCase();
     return userInput;
 }
 
-/*console.log(getHumanChoice())
-
 
 /* States Player Scores */
-let humanScore = 0;
-let computerScore = 0;
+var humanScore = 0;
+var computerScore = 0;
 
 
+/* Logic to play one Round */
+function playRound(humanChoice, computerChoice) {
+   
+    if (humanChoice === computerChoice){
+        console.log("It's a tie")
 
+    } else if ((humanChoice === "rock" && computerChoice === "Paper")
+               || (humanChoice === "paper" && computerChoice === "Scissor")
+               || (humanChoice === "scissor" && computerChoice === "Rock"))
+            { 
+                computerScore++;
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`); 
+
+    }else if ((humanChoice === "paper" && computerChoice === "Rock")
+        || (humanChoice === "scissor" && computerChoice === "Paper")
+        || (humanChoice === "rock" && computerChoice === "Scissor"))
+     {
+        humanScore++;
+ console.log(`You Win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`); 
+    }else{ console.log ("Invalid choice!\nPlease enter Rock, Paper, or Scissor")}
+        }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
